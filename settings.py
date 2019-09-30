d35948e2b051645fa0bb504a33bfecc71e241de6
isspek <ipekbrs@gmail.com>
2019-09-30 09:06:14 +0200

    use INI sections to configure multiple API keys

--- settings.py
+++ settings.py
@@ -1,20 +1,12 @@
 # settings.py
 import os
 import configparser
-from dotenv import load_dotenv
-from dotenv import parser
-# load_dotenv()
-
-# OR, the same with increased verbosity:
-# load_dotenv(verbose=True)
-
 
 # OR, explicitly providing path to '.env'
 from pathlib import Path  # python3 only
 env_path = Path('.') / 'config.ini'
 config = configparser.ConfigParser()
 config.read(env_path)
-# load_dotenv(dotenv_path=env_path)
 
 def convert_str(tup):
     str =  ''.join(tup)
@@ -31,39 +23,13 @@ def convert_str(tup):
 #     auth.set_access_token(access_key, access_secret)
 #     auths.append(auth)
 
-
-def get_twitter_apis():
-    auths = []
-    auth = []
-    num_api = 0
-    for key, value in config.items('Twitter API Settings'):
-        print(key)
-        print("twitter_{}_".format(num_api))
-        if "twitter_{}_".format(num_api) in key:
-            print("yes")
-            auth.append(value)
-        else:
-            auths.append(auth)
-            num_api = num_api + 1
-    return auths
-
-apis = get_twitter_apis()
-for api in apis:
-    print(api)
-
-def get_consumer_key():
-    return os.environ['CONSUMERKEY']
-
-def get_consumer_secret():
-    return os.environ['CONSUMERSECRET']
-
-
-def get_access_token():
-    return os.environ['ACCESSTOKEN']
-
-
-def get_acess_tokensec():
-    return os.environ['ACCESSTOKENSEC']
+def get_twitter_credentials( ini_path ):
+    config = configparser.ConfigParser()
+    config.read(ini_path)
+    sections = [s for s in config.sections() if s.startswith('Twitter API ')]
+    return [{
+        k.upper(): config[s][k] for k in config[s]
+    } for s in sections]
 
 def get_num_replies():
     return config.getint('Other Settings','NUM_REPLIES')
