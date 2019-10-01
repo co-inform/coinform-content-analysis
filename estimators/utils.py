2bdeb2690839b8a5a7a6da21d8ece59a3a4b45c5
isspek <ipekbrs@gmail.com>
2019-10-01 09:55:58 +0200

    clean up

--- estimators/utils.py
+++ estimators/utils.py
@@ -4,7 +4,11 @@ CUDA = 'cuda'
 CPU = 'cpu'
 
 
+def get_device():
+    return torch.device(CUDA) if torch.cuda.is_available() else torch.device(CPU)
+
+
 def load_model_from_file(file_path: str) -> torch.nn.Module:
-    dev = torch.device(CUDA) if torch.cuda.is_available() else torch.device(CPU)
-    model = torch.load(file_path, map_location=dev)
+    model = torch.load(file_path, map_location=get_device())
     return model
+
