c1e97adb425f1bfd834c89fcc997f0262786d059
isspek <ipekbrs@gmail.com>
2019-10-02 09:42:16 +0200

    added clearumor models

--- estimators/clearumor.py
+++ estimators/clearumor.py
@@ -3,7 +3,7 @@ import estimators.utils as utils
 import src.dataset as dataset
 import src.util as util
 import src.sdqc as sdqc
-import service.twitter_service as twitter_service
+import numpy as np
 from torch import nn
 from torch import from_numpy
 from torch import argmax
@@ -13,10 +13,11 @@ from pathlib import Path
 
 
 class ClearRumorModel:
-    model = None
+    stance_model = None
 
-    def __init__(self, model:nn.Module):
-        self.model = model
+    def __init__(self, stance_model:nn.Module, verification_model:nn.Module):
+        self.stance_model = stance_model
+        self.verification_model = verification_model
         self.device = utils.get_device()
         #todo read model parameters from the model
         self.max_sentence_length = 32
@@ -67,20 +68,26 @@ class ClearRumorModel:
                         (sdqc.SdqcInstance.Label(prediction.item()),
                          dict(zip(sdqc.SdqcInstance.Label, probs)))
 
-        return estimations[src_tweet_id], estimations
+        return estimations[src_tweet_id], estimations, posts, post_embeddings
 
 
+    def estimate_veracity(self, src_tweet_id, conversation):
+        # todo check if we have already stance estimations, if not run stance, because the model depend on stances
+        _,estimations, posts, post_embeddings =self.estimate_stance(src_tweet_id, conversation)
+        self.verification_model.eval()
+        post_features = self.verification_mode.Verif.Dataset.calc_features(estimations[src_tweet_id], posts, post_embeddings,
+                                                          estimations)
 
-# model = torch.load(env_path, map_location='cpu')
-# model.eval()
-#
-# results = {}
-# for key, post in replies.items():
-#     post_features = sdqc.Sdqc.Dataset.calc_features(post, post_embeddings)
-#     logits = model(post_embeddings[key].unsqueeze(0), torch.from_numpy(post_features).unsqueeze(0))
-#     probs = F.softmax(logits, dim=1).squeeze().tolist()
-#     prediction = torch.argmax(logits, dim=1)
-#
-#     results[key] = \
-#                 (sdqc.SdqcInstance.Label(prediction.item()),
-#                  dict(zip(sdqc.SdqcInstance.Label, probs)))
+        logits = self.verification_model(from_numpy(post_features).unsqueeze(0))
+        prediction_probs, batch_predictions =F.softmax(logits, dim=1).max(dim=1)
+        predictions = batch_predictions.data.cpu().numpy()
+        prediction_probs = \
+            prediction_probs.data.cpu().numpy()
+
+        batch_confidences = np.maximum(0.5, prediction_probs)
+        batch_confidences[batch_predictions
+                          == self.verification_model.VerifInstance.Label.unverified.value] = 0
+        batch_predictions[batch_predictions
+                          == self.verification_model.VerifInstance.Label.unverified.value] = \
+            self.verification_model.VerifInstance.Label.true.value
+        return logits, batch_confidences, batch_predictions
