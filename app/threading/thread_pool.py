0a23f0ff061a14ec7f184a65e0acfd68eb6bd83c
Andreas <andreas.berg@dsv.su.se>
2019-11-18 10:30:50 +0100

    locking the set

--- app/threading/thread_pool.py
+++ app/threading/thread_pool.py
@@ -3,11 +3,13 @@ import requests
 import concurrent.futures.thread
 from fastapi import HTTPException
 import queue
+import threading
 
 log = logging.getLogger('server')
 
 # structure for keeping what tweets the threads are actually working on. dont know if it needs locking or not in concurrency
 tweet_set = set({})
+set_lock = threading.Lock()
 # the different queues for working on tweets, conversations etc
 received_tweet_queue = queue.Queue(maxsize=0)
 content_analysis_queue = queue.Queue(maxsize=0)
@@ -16,12 +18,13 @@ callback_queue = queue.Queue(maxsize=0)
 
 def tweet_queue_consumer():
     while True:
-        d = received_tweet_queue.get()
+        d = received_tweet_queue.get(block=True)
         log.info(str(d['callback_url']))
 
         conversation = d['connector'].get_conversation(d['tweet_id'])
         if conversation is None:
-            tweet_set.discard(d['tweet_id'])
+            with set_lock:
+                tweet_set.discard(d['tweet_id'])
             raise IOError('Unable to get Twitter conversation')
 
         content_analysis_queue.put({"tweet_id": d['tweet_id'],
@@ -32,11 +35,12 @@ def tweet_queue_consumer():
 
 def content_queue_consumer():
     while True:
-        d = content_analysis_queue.get()
+        d = content_analysis_queue.get(block=True)
 
         results = d['model'].estimate_veracity(conversation=d['conversation'])
         if results is None:
-            tweet_set.discard(d['tweet_id'])
+            with set_lock:
+                tweet_set.discard(d['tweet_id'])
             raise Exception('Unable to compute results from conversation')
 
         callback_queue.put({"tweet_id": d['tweet_id'],
@@ -46,7 +50,7 @@ def content_queue_consumer():
 
 def callback_queue_consumer():
     while True:
-        d = callback_queue.get()
+        d = callback_queue.get(block=True)
 
         try:
             result = requests.post(url=d['callback_url'],
@@ -55,47 +59,29 @@ def callback_queue_consumer():
         except requests.exceptions.RequestException as exc:
             log.error('Request error: {}'.format(exc))
         finally:
-            tweet_set.discard(d['tweet_id'])
+            with set_lock:
+                tweet_set.discard(d['tweet_id'])
 
 
 class ThreadPool:
-    def __init__(self):
+    def __init__(self, num_threads=8):
         log.info('Thread pool created')
 
-        # todo: maybe change the amount of workers for each ThreadPool?
-        # kanske snyggare lösning än den nedanför?
-        #        with concurrent.futures.thread.ThreadPoolExecutor(max_workers=24) as executor:
-        #            for n in range(start=1, stop=8):
-        #                executor.submit(tweet_queue_consumer)
-        #                executor.submit(content_queue_consumer)
-        #                executor.submit(callback_queue_consumer)
-
-        self.fetchtweet_worker_pool = concurrent.futures.thread.ThreadPoolExecutor(max_workers=8,
-                                                                                   thread_name_prefix='FetchTweetWorkerPool-')
-        self.content_worker_pool = concurrent.futures.thread.ThreadPoolExecutor(max_workers=8,
-                                                                                thread_name_prefix='ContentWorkerPool-')
-        self.callback_worker_pool = concurrent.futures.thread.ThreadPoolExecutor(max_workers=8,
-                                                                                 thread_name_prefix='CallbackWorkerPool-')
-
-        for n in range(start=1, stop=8):
-            self.fetchtweet_worker_pool.submit(tweet_queue_consumer)
-            self.content_worker_pool.submit(content_queue_consumer)
-            self.callback_worker_pool.submit(callback_queue_consumer)
-
-    def __del__(self):
-        self.callback_worker_pool.shutdown()
-        self.content_worker_pool.shutdown()
-        self.fetchtweet_worker_pool.shutdown()
+        with concurrent.futures.thread.ThreadPoolExecutor(max_workers=3 * num_threads) as executor:
+            for n in range(start=1, stop=num_threads):
+                executor.submit(tweet_queue_consumer)
+                executor.submit(content_queue_consumer)
+                executor.submit(callback_queue_consumer)
 
     def add(self, connector, tweet_id, model, callback_url):
         log.info('using pool process to execute twitter call async')
 
-        # todo: cant find if a set is concurrent in Python. Maybe needs a lock for thread safety?
-        if tweet_id in tweet_set:
-            raise HTTPException(status_code=400,
-                                detail='Tweet id is already in process')
+        with set_lock:
+            if tweet_id in tweet_set:
+                raise HTTPException(status_code=400,
+                                    detail='Tweet id is already in process')
 
-        tweet_set.add(tweet_id)
+            tweet_set.add(tweet_id)
         received_tweet_queue.put({"tweet_id": tweet_id,
                                   "connector": connector,
                                   "model": model,
