2a078dc0d0dce5b6a01684c28c6c1b1a9e6bd464
isspek <ipekbrs@gmail.com>
2019-12-18 22:40:48 +0100

    fixed no reply

--- src/estimators/baseline.py
+++ src/estimators/baseline.py
@@ -58,18 +58,18 @@ class BaselineModel:
                 feats_replies.append(
                     self.feat_extractor.extract_aux_feats(replies[i], self.aux_feats, source_id=source['id'], prev_id=replies[i-1]['id'],
                                                           embeddings=embeddings,post_type = 0))
+        if len(feats_replies) > 0:
+            feats_replies = np.concatenate([bow_replies, feats_replies], axis=1)
 
-        feats_replies = np.concatenate([bow_replies, feats_replies], axis=1)
-
-        feats_replies = np.where(np.isnan(feats_replies), 0, feats_replies)
-        log.info(self.stance_model.predict_proba(feats_replies))
-        log.info(self.stance_model.predict(feats_replies))
-        replies_stance_dict = dict(zip(replies_id,self.stance_model.predict_proba(feats_replies)))
-        for id, value in replies_stance_dict.items():
-            replies_response[id]['stance_comment'] = value[0]
-            replies_response[id]['stance_deny'] =  value[1]
-            replies_response[id]['stance_query']=value[2]
-            replies_response[id]['stance_support']= value[3]
+            feats_replies = np.where(np.isnan(feats_replies), 0, feats_replies)
+            log.info(self.stance_model.predict_proba(feats_replies))
+            log.info(self.stance_model.predict(feats_replies))
+            replies_stance_dict = dict(zip(replies_id,self.stance_model.predict_proba(feats_replies)))
+            for id, value in replies_stance_dict.items():
+                replies_response[id]['stance_comment'] = value[0]
+                replies_response[id]['stance_deny'] =  value[1]
+                replies_response[id]['stance_query']=value[2]
+                replies_response[id]['stance_support']= value[3]
 
         log.info(np.asarray(bow_source).shape)
         log.info(feats_source.shape)
@@ -84,13 +84,18 @@ class BaselineModel:
         source_response['stance_support'] = probs[3]
 
 
-
-        source_response['avg_stance_comment'] = float(sum(value[0] for value in replies_stance_dict.values())/replies_stance_dict.values().__len__())
-        source_response['avg_stance_deny'] = float(sum(value[1] for value in replies_stance_dict.values())/replies_stance_dict.values().__len__())
-        source_response['avg_stance_query'] = float(sum(value[2] for value in replies_stance_dict.values())/replies_stance_dict.values().__len__())
-        source_response['avg_stance_support'] = float(sum(value[3] for value in replies_stance_dict.values())/replies_stance_dict.__len__())
-        avg_stances = [source_response['avg_stance_comment'],source_response['avg_stance_deny'],source_response['avg_stance_query'],source_response['avg_stance_support']]
-
+        if len(feats_replies) > 0:
+            source_response['avg_stance_comment'] = float(sum(value[0] for value in replies_stance_dict.values())/replies_stance_dict.values().__len__())
+            source_response['avg_stance_deny'] = float(sum(value[1] for value in replies_stance_dict.values())/replies_stance_dict.values().__len__())
+            source_response['avg_stance_query'] = float(sum(value[2] for value in replies_stance_dict.values())/replies_stance_dict.values().__len__())
+            source_response['avg_stance_support'] = float(sum(value[3] for value in replies_stance_dict.values())/replies_stance_dict.__len__())
+        else:
+            source_response['avg_stance_comment'] = 0.0
+            source_response['avg_stance_deny'] = 0.0
+            source_response['avg_stance_query'] = 0.0
+            source_response['avg_stance_support'] = 0.0
+        avg_stances = [source_response['avg_stance_comment'], source_response['avg_stance_deny'],
+                       source_response['avg_stance_query'], source_response['avg_stance_support']]
         avg_stances = np.asarray(avg_stances).reshape(1,-1)
         log.info(avg_stances.shape)
         log.info(feats_source.shape)
--- src/service/twitter_service.py
+++ src/service/twitter_service.py
@@ -15,8 +15,21 @@ class TwitterService:
 
     def get_conversation(self, tweetId: str) -> Dict:
         twitter_conn = settings.get_twitter_connector()
-        url = "http://" + twitter_conn['host'] + ":" + twitter_conn['port'] + "/tweets/conversation/" + tweetId
-        response = requests.get(url)
+        url_conversation = "http://" + twitter_conn['host'] + ":" + twitter_conn['port'] + "/tweets/conversation/" + tweetId
+        response = requests.get(url_conversation)
+        if response.status_code is not 200:
+            try:
+                return {'source': self.get_tweet(tweetId),
+                        'replies': []}
+            except:
+              raise requests.HTTPError
+        else:
+            return json.loads(response.content)
+
+    def get_tweet(self,tweetId:str) -> Dict:
+        twitter_conn = settings.get_twitter_connector()
+        url_tweet = "http://" + twitter_conn['host'] + ":" + twitter_conn['port'] + "/tweets/" + tweetId
+        response = requests.get(url_tweet)
         if response.status_code is not 200:
             raise requests.HTTPError
         else:
