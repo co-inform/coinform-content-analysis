9c5dc110212542d0a4e1cbc2a874df4ba28a0de0
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2020-08-10 16:53:24 +0200

    BERT for ca in progress

--- src/estimators/simple_bert.py
+++ src/estimators/simple_bert.py
@@ -1,11 +1,11 @@
 import logging
 
-from typing import Union, List
+from typing import Union
 
 import torch
 import torch.optim as optim
 import numpy as np
-from transformers import BertConfig, BertForSequenceClassification, BertTokenizer
+from transformers import BertForSequenceClassification, BertTokenizer
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 
 from sklearn.metrics import precision_recall_fscore_support
@@ -15,25 +15,22 @@ import pandas as pd
 
 from tqdm import tqdm
 
-
 log = logging.getLogger('server')
 
 
 class SimpleBERT:
     def __init__(self, model_path: str):
-        config = BertConfig(num_labels=3)
-        model = BertForSequenceClassification(config)
-        torch.load(model, model_path)
+        model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=3)
+        model.load_state_dict(torch.load("model/bert_4_28_test.pt"))
 
         self.model = model
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
     def estimate_veracity(self, conversation):
         source = conversation['source']
-        labels = ['true', 'false', 'unverified']
 
-        trainer = Trainer(self.model, self.tokenizer, labels)
-        credibility, conf = trainer.predict(None, source)
+        trainer = Trainer(self.model, self.tokenizer)
+        credibility, conf = trainer.predict(None, source['text'])
 
         source_response = {'id': source['id'], 'text': source['text']}
 
@@ -49,7 +46,6 @@ class Trainer:
             self,
             model: BertForSequenceClassification,
             tokenizer: BertTokenizer,
-            labels: List[str],
             max_seq_length: int = 280,
             device: Union[torch.device, str] = 'cpu'
     ):
@@ -57,20 +53,20 @@ class Trainer:
         self.model = model
         self.device = device
         self.tokenizer = tokenizer
-        self.label2idx = {label: idx for (idx, label) in enumerate(labels)}
-        self.idx2label = {idx: label for (idx, label) in enumerate(labels)}
+        self.label2idx = {"true": 1, "false": 0, "unverified": 2}
+        self.idx2label = {0: "false", 1: "true", 2: "unverified"}
 
     def train(self, train_set, valid_set, batch_size, num_epochs):
         train_text, train_labels = train_set['text'], train_set['label']
 
         tokens_train = self.tokenizer.batch_encode_plus(
-            train_text.tolist(),
+            train_text.values,
             max_length=self.max_seq_length,
             pad_to_max_length=True,
             truncation=True
         )
 
-        enc_labels = [self.label2idx[x] for x in train_labels.tolist()]
+        enc_labels = [self.label2idx[x] for x in train_labels.values]
 
         train_seq = torch.tensor(tokens_train['input_ids'])
         train_mask = torch.tensor(tokens_train['attention_mask'])
@@ -83,13 +79,13 @@ class Trainer:
         valid_text, valid_labels = valid_set['text'], valid_set['label']
 
         tokens_valid = self.tokenizer.batch_encode_plus(
-            valid_text.tolist(),
+            valid_text.values,
             max_length=self.max_seq_length,
             pad_to_max_length=True,
             truncation=True
         )
 
-        enc_labels_val = [self.label2idx[x] for x in valid_labels.tolist()]
+        enc_labels_val = [self.label2idx[x] for x in valid_labels.values]
 
         val_seq = torch.tensor(tokens_valid['input_ids'])
         val_mask = torch.tensor(tokens_valid['attention_mask'])
@@ -112,6 +108,7 @@ class Trainer:
                 loss, data = model(input_ids=x, attention_mask=m, labels=y)
 
                 loss.backward()
+                # print("\nloss: " + str(loss))
                 optimizer.step()
 
             print("\nEpoch: " + str(epoch) + "__________________________\n")
@@ -123,21 +120,20 @@ class Trainer:
             torch.save(model.state_dict(), "model/bert_" + str(epoch) + ".pt")
 
     def predict(self, dataloader, text):
-        if text is not None: # todo: refactor :/
-            tokens = self.tokenizer.batch_encode_plus(
-                [text],
+        if text is not None:  # todo: refactor :/
+            tokens = self.tokenizer.encode_plus(
+                text,
                 max_length=self.max_seq_length,
                 pad_to_max_length=True,
                 truncation=True
             )
 
             seq = torch.tensor(tokens['input_ids'])
-            mask = torch.tensor(tokens['attention_mask'])
 
             with torch.no_grad():
                 self.model.eval()
 
-                data = self.model(input_ids=seq, attention_mask=mask)
+                data = self.model(input_ids=seq)
 
                 sm = softmax(data)
 
@@ -173,19 +169,65 @@ class Trainer:
 
 
 def main():
-    labels = ['true', 'false', 'unverified']
+    # data
+    df_re = pd.read_csv("rumeval.tsv", sep='\t')
+    df = pd.read_csv("tweeter1516.tsv", sep='\t')
+    df_dev = df.sample(frac=0.1)  # use as the development set
+    df = df.loc[~df.index.isin(df_dev.index)]
 
-    df = pd.read_csv("rumeval.tsv", sep='\t')
-    df_dev = pd.read_csv("rumeval_dev.tsv", sep='\t')
+    df_test = df.sample(frac=0.1)
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+    df = df.loc[~df.index.isin(df_dev.index)]
 
-    config = BertConfig(num_labels=3)
-    model = BertForSequenceClassification(config)
+    df = pd.concat([df_re, df], axis=0, sort=False, join='inner')
 
-    trainer = Trainer(model, tokenizer, labels)
-    trainer.train(df, df_dev, 8, 10)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # train
+    model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=3)
+
+    trainer = Trainer(model, tokenizer)
+    trainer.train(df, df_dev, 8, 5)
+
+    # test
+    df_dev_re = pd.read_csv("rumeval_dev.tsv", sep='\t')  # use for test after model selection
+    df_dev_re = pd.concat([df_dev_re, df_test], axis=0, sort=False, join='inner') # 10 percent of twitter 15-16 + rumeval dev
+    model.load_state_dict(torch.load("model/bert_4.pt"))
+
+    text, labels = df_dev_re['text'], df_dev_re['label']
+
+    tokens = tokenizer.batch_encode_plus(
+        text.values,
+        max_length=280,
+        pad_to_max_length=True,
+        truncation=True
+    )
+
+    enc_labels = [trainer.label2idx[x] for x in labels.values]
+
+    seq = torch.tensor(tokens['input_ids'])
+    mask = torch.tensor(tokens['attention_mask'])
+    y = torch.tensor(enc_labels)
+
+    data = TensorDataset(seq, mask, y)
+    sampler = SequentialSampler(data)
+    dataloader = DataLoader(data, sampler=sampler, batch_size=8)
+
+    results = trainer.evaluate(dataloader, enc_labels)
+
+    for key in sorted(results.keys()):
+        print(key, str(results[key]))
+
+    # rumeval dev
+    # f1 0.6349206349206349
+    # precision 0.6349206349206349
+    # recall 0.6666666666666666
 
 
 if __name__ == '__main__':
     main()
+    # m = SimpleBERT("")
+    # rsp = m.estimate_veracity({'source': {
+    #     "id": 1,
+    #     "text": 'Very tense situation in Ottawa this morning.  Multiple gun shots fired outside of our caucus room.  I am safe and in lockdown. Unbelievable.'}})
+    # print()
